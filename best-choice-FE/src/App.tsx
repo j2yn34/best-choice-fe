f678b31c68229a9baadb06dbb8993306312dbfea
Hansebin <sebinn052@gmail.com>
2023-08-14 14:54:03 +0900

    Test : PR 테스트 - PR 테스트를 위해 App.tsx 파일에 주석 추가

--- best-choice-FE/src/App.tsx
+++ best-choice-FE/src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+// PR 및 Merge 테스트 - 한세빈
+
 function App() {
   const [count, setCount] = useState(0);
 
