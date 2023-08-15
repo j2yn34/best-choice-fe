3542790111a732fbeaf5e35275ca527c59310a25
Hansebin <sebinn052@gmail.com>
2023-08-15 21:26:24 +0900

    Feat: header, footer 컴포넌트 생성 - header 컴포넌트 생성 후 라우터 연결 - footer 컴포넌트 생성 후 라우터 연결 - react-icons 사용 => 라이브러리 설치 필요

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { AiFillGithub } from "react-icons/ai";
 
 const Footer = (): JSX.Element => {
-  return <div>Footer!</div>;
+  return (
+    <div className="w-full bg-black-primary flex flex-row items-center justify-center h-24">
+      <Link
+        to="https://github.com/winnow-2023?tab=repositories"
+        target="_blank"
+        className="text-white mr-5 text-2xl"
+      >
+        <AiFillGithub />
+      </Link>
+      <p className="text-white font-light">
+        Copyright © 2023 Best-Choice All rights reserved
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { IoMdNotifications } from "react-icons/io";
 
 const Header = (): JSX.Element => {
-  return <div>Header!</div>;
+  const leftMenus = [
+    { name: "posts", title: "투표글" },
+    { name: "hot", title: "HOT" },
+    { name: "chat", title: "채팅방" },
+    { name: "search", title: "검색" },
+  ];
+
+  return (
+    <div className="w-ful bg-white flex flex-row items-center h-[70px] shadow-md">
+      <div className="xl:container mx-auto flex flex-row justify-between">
+        <div>
+          <Link to="/" className="mr-10">
+            로고
+          </Link>
+          {leftMenus.map((menu) => (
+            <Link
+              to={`/${menu.name}`}
+              key={menu.name}
+              className="text-base mr-6"
+            >
+              {menu.title}
+            </Link>
+          ))}
+        </div>
+        <div className="flex flex-row items-center">
+          <Link to="/notification" className="text-2xl ml-6">
+            <IoMdNotifications />
+          </Link>
+          <Link to="/member" className="text-base ml-6">
+            회원페이지
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
