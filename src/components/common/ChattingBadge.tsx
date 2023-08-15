6c463e2339f2a19984ee2ca72cb65692e97b54ce
Jiyeon Park <wldusys34@gmail.com>
2023-08-16 00:33:45 +0900

    Feat: 채팅중, 채팅인원 배지 컴포넌트 생성

---
+++ src/components/common/ChattingBadge.tsx
@@ -0,0 +1,9 @@
+const ChattingBadge = () => {
+  return (
+    <div className="badge border-0 bg-black-primary text-white w-[62px] h-[30px]">
+      채팅중
+    </div>
+  );
+};
+
+export default ChattingBadge;
