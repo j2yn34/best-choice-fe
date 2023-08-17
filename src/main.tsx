ac514736e17c06135a6080bfa2248aa56adb90e3
Hansebin <sebinn052@gmail.com>
2023-08-17 15:14:31 +0900

    Feat: fetch hook 함수 생성
    - 여러 페이지에서 데이터 요청을 하기 위해 axion 사용
    - 반복되는 코드 사용을 줄이기 위해 하나의 fetch hook 함수 생성
    - 데이터 요청 시 해당 hook 함수 불러와서 인자 넘겨주면 사용 가능

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClientProvider, QueryClient } from "react-query";
 import App from "./App.tsx";
 import "./assets/style.css";
 import { worker } from "./mocks/worker.ts";
@@ -8,8 +9,12 @@ if (process.env.NODE_ENV === "development") {
   worker.start();
 }
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
