8ebc7fafafb46669842dccfdd92c74ac3c1b298f
Hansebin <sebinn052@gmail.com>
2023-08-17 16:27:17 +0900

    Feat: 메인 페이지 데이터 조회 및 정렬, 글쓰기 버튼 추가
    - fetch hook 함수 사용해서 투표글 데이터 조회
    - PostCardList 컴포넌트의 sort props에 true 값을 넘겨서 정렬 기능과 글쓰기 버튼이 보이도록 설정

--- src/views/PostListPage.tsx
+++ src/views/PostListPage.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import useFetchData from "../hooks/useFetchData";
+import PostCardList from "../components/contents/PostCardList";
 
 const PostListPage = (): JSX.Element => {
-  return <div>PostListPage!</div>;
+  const {
+    isLoading,
+    data: postData,
+    isError,
+  } = useFetchData("/postListData", ["postData"]);
+
+  if (isError) {
+    console.log("데이터 불러오기 실패");
+  }
+
+  return (
+    <>
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <PostCardList postData={postData} title="투표글" sort={true} />
+      )}
+    </>
+  );
 };
 
 export default PostListPage;
