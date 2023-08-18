c978ee2ac6151d2d81a9d84761cf5026f5f1ca92
Hansebin <sebinn052@gmail.com>
2023-08-18 11:52:47 +0900

    Refactor: ReactQueryDevtools 추가
    - ReactQueryDevtools main.tsx에 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import App from "./App.tsx";
 import "./assets/style.css";
 import { worker } from "./mocks/worker.ts";
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
