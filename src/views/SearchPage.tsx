9003617a25ff96ce6759e5df218c3c61caa59d8e
Hansebin <sebinn052@gmail.com>
2023-08-19 17:22:24 +0900

    Feat: 검색 페이지 구현
    - 검색 페이지 레이아웃 구성
    - 검색한 결과 '결과 타이틀', 콘솔에 보여지도록 구현

--- src/views/SearchPage.tsx
+++ src/views/SearchPage.tsx
@@ -1,5 +1,40 @@
+import React from "react";
+import { useState } from "react";
+
 const SearchPage = (): JSX.Element => {
-  return <div>SearchPage!</div>;
+  const [inputValue, setInputValue] = useState<string>("");
+  const [submitValue, setSubmitValue] = useState<string>("");
+
+  const searchClick = (e: React.FormEvent) => {
+    e.preventDefault();
+    setSubmitValue(inputValue);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setInputValue(value);
+  };
+
+  return (
+    <>
+      <h1 className="text-2xl mb-8 font-semibold">게시글 태그 검색</h1>
+      <form onSubmit={searchClick} className="flex justify-center items-center">
+        <input
+          value={inputValue}
+          type="text"
+          className="rounded-lg h-[50px] w-4/5 md:w-3/5 pl-3 focus:outline-none"
+          onChange={handleChange}
+          onFocus={() => {
+            setInputValue("");
+          }}
+        />
+        <button className="px-3 py-3 md:px-5 bg-black-primary text-white text-base md:text-xl rounded-lg ml-[14px]">
+          검색
+        </button>
+      </form>
+      <p className="text-xl mt-8">#{submitValue} 검색 결과(0)</p>
+    </>
+  );
 };
 
 export default SearchPage;
