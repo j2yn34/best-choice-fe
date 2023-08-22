6a404124f6bbe235e9f01680d3c2a9f748ae5f84
Jiyeon Park <wldusys34@gmail.com>
2023-08-22 18:15:31 +0900

    Feat: msw 알림 mock 데이터 생성 및 handler 조회 함수 추가
    - 알림 데이터 생성 및 타입 설정
    - 알림 데이터 handler 조회 함수 작성
    
    To do
    - pagable 관련 데이터 작성 필요

---
+++ src/mocks/mockDatas/notificationData.ts
@@ -0,0 +1,20 @@
+import { NotificationData } from "../mockType";
+
+export const notificationData: NotificationData = {
+  content: [
+    {
+      notificationId: 2,
+      checked: false,
+      createdDate: "1분 전",
+      postId: 2,
+      postTitle: "소개팅 장소 어디가 좋을까요?",
+    },
+    {
+      notificationId: 1,
+      checked: true,
+      createdDate: "2023.08.11",
+      postId: 5,
+      postTitle: "요즘 날씨에 어울리는 옷은?",
+    },
+  ],
+};
