05a552348ef6cff90868d3d02bcd296539b93ec9
Hansebin <sebinn052@gmail.com>
2023-08-22 17:28:30 +0900

    Feat: 미리보기 추가
    - 동영상 미리보기 기능 추가

--- src/components/common/FileInput.tsx
+++ src/components/common/FileInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { useRecoilState, RecoilState } from "recoil";
 import { inputValueState } from "../../states/recoil";
 import { InputValue } from "../../states/recoilType";
@@ -7,11 +7,12 @@ const maxFileCnt = 5;
 
 const FileInput = () => {
   const [showImages, setShowImages] = useState<string[]>([]);
+  const [showVideos, setShowVideos] = useState<string[]>([]);
   const [inputValue, setInputValue] = useRecoilState(
     inputValueState as RecoilState<InputValue>
   );
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
 
     if (files && files?.length > maxFileCnt) {
@@ -20,14 +21,19 @@ const FileInput = () => {
     }
 
     if (files) {
-      const fileList = Array.from(files);
-
       setShowImages([]);
 
       for (let i = 0; i < files.length; i++) {
-        const currentImageUrl = URL.createObjectURL(files[i]);
-        setShowImages((prevImages) => [...prevImages, currentImageUrl]);
+        if (files[i].type.includes("video")) {
+          const currentVideoUrl = URL.createObjectURL(files[i]);
+          setShowVideos((prevImages) => [...prevImages, currentVideoUrl]);
+        } else {
+          const currentImageUrl = URL.createObjectURL(files[i]);
+          setShowImages((prevImages) => [...prevImages, currentImageUrl]);
+        }
       }
+
+      const fileList = Array.from(files);
 
       setInputValue((prevInputValues) => ({
         ...prevInputValues,
@@ -46,10 +52,15 @@ const FileInput = () => {
         onChange={handleFileChange}
         className="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:cursor-pointer file:border-0 file:text-sm file:font-semibold file:bg-color-bg file:text-black"
       />
-      <div className="flex items-center gap-5 mt-3">
-        {showImages.slice(0, 5).map((image, id) => (
+      <div className="flex items-center md:gap-5 mt-3 gap-1">
+        {showImages.map((image, id) => (
           <div key={id}>
             <img src={image} alt={`${image}-${id}`} width={300} />
+          </div>
+        ))}
+        {showVideos.map((video, id) => (
+          <div key={id}>
+            <video src={video} controls width={300} />
           </div>
         ))}
       </div>
