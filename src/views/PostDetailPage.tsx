12911c68b20e5d8be5faee71c39a9a3421bbc253
Jiyeon Park <wldusys34@gmail.com>
2023-08-23 15:14:01 +0900

    Feat: 투표글 상세페이지 구현
    - 투표글 상세페이지 구현 및 데이터 연동
    - 추천(좋아요) 버튼 컴포넌트 구현
    - 투표 그래프 구현
    
    To do
    - 추천 버튼 기능 구현
    - 투표 그래프에 데이터 연동

--- src/views/PostDetailPage.tsx
+++ src/views/PostDetailPage.tsx
@@ -1,5 +1,18 @@
+import { useParams } from "react-router-dom";
+import PostDetail from "../components/contents/PostDetail";
+
 const PostDetailPage = (): JSX.Element => {
-  return <div>PostDetailPage!</div>;
+  const { postId } = useParams<{ postId?: string }>();
+
+  if (!postId) {
+    return <div>페이지를 찾을 수 없습니다.</div>;
+  }
+
+  return (
+    <>
+      <PostDetail postId={postId} />
+    </>
+  );
 };
 
 export default PostDetailPage;
