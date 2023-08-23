2a87a5131b55052b1cb74876b083f8f7a6f61478
Hansebin <sebinn052@gmail.com>
2023-08-23 14:20:22 +0900

    Feat: 댓글 컴포넌트 생성 및 댓글 데이터 post 테스트
    - PostDetailPage에 댓글 컴포넌트 추가
    - 댓글 컴포넌트 내부에서 input과 list 분리 -> input의 경우 post 로직이 길어질 수 있을 것 같아서 따로 분리해서 관리하는 게 좋을 것 같음
    - 댓글 추가 테스트 -> 임시 post 로직으로 테스트 / 추후에 변경될 예정

---
+++ src/components/common/CommentInput.tsx
@@ -0,0 +1,82 @@
+import axios from "axios";
+import { useState, ChangeEvent, FormEvent } from "react";
+import { useMutation, useQueryClient } from "react-query";
+import { Comment } from "../../mocks/mockType";
+
+const CommentInput = () => {
+  // 임시 state
+  const [commentValue, setCommentValue] = useState<Comment>({
+    commentId: 12345,
+    member: { memberId: 12345, nickname: "test" },
+    option: "T",
+    content: "",
+    createdDate: "테스트용",
+    deletedDate: null,
+    likeCount: 0,
+  });
+
+  const onchangeComment = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = e.target;
+
+    setCommentValue((prevInputValues) => ({
+      ...prevInputValues,
+      content: value,
+    }));
+  };
+
+  // 임시 post
+  const queryClient = useQueryClient();
+
+  const addComment = (commentValue: Comment) => {
+    return axios.post("/commentListData", commentValue);
+  };
+
+  const {
+    mutate: addCommentData,
+    isLoading,
+    isError,
+  } = useMutation(addComment, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("commentData");
+    },
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>에러 발생</div>;
+  }
+
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    addCommentData(commentValue);
+
+    const CleanCommentValue = { ...commentValue };
+    CleanCommentValue.content = "";
+    setCommentValue(CleanCommentValue);
+  };
+
+  return (
+    <>
+      <form
+        onSubmit={(e) => onSubmit(e)}
+        className="flex items-center justify-between gap-2 md:gap-5 w-full bg-color-bg p-3 md:p-5 h-[120px] rounded-lg mt-16"
+      >
+        <textarea
+          value={commentValue.content}
+          placeholder="댓글을 입력해 주세요."
+          className="w-full h-full p-4 focus:outline-none"
+          onChange={(e) => onchangeComment(e)}
+          required
+        ></textarea>
+        <button className="btn bg-black-primary hover:bg-black text-white">
+          확인
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default CommentInput;
