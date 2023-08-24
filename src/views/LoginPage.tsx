36210eb5c3ff1fa3bbd5d9c567374313d6aef5ef
Jiyeon Park <wldusys34@gmail.com>
2023-08-25 02:56:07 +0900

    Add: 로그인 페이지 레이아웃 구현
    - 소셜 로그인 버튼 생성

--- src/views/LoginPage.tsx
+++ src/views/LoginPage.tsx
@@ -1,5 +1,35 @@
+import { FcGoogle } from "react-icons/fc";
+import { RiKakaoTalkFill } from "react-icons/ri";
+import { SiNaver } from "react-icons/si";
+
 const LoginPage = (): JSX.Element => {
-  return <div>LoginPage!</div>;
+  return (
+    <div className="fixed inset-0 flex items-center justify-center mb-24">
+      <div className="flex flex-col items-center gap-12">
+        <span>로고</span>
+        <div className="flex flex-col gap-4">
+          <button className="btn w-64 h-14 border-0 bg-white hover:bg-blue-100">
+            <span className="text-lg">
+              <FcGoogle />
+            </span>
+            <span className="text-base">구글 아이디로 시작</span>
+          </button>
+          <button className="btn w-64 h-14 border-0 bg-[#fee500] hover:bg-[#fada0a]">
+            <span className="text-lg">
+              <RiKakaoTalkFill />
+            </span>
+            <span className="text-base">카카오 아이디로 시작</span>
+          </button>
+          <button className="btn w-64 h-14 border-0 bg-[#03c75a] hover:bg-[#17b75e]">
+            <span className="text-white">
+              <SiNaver />
+            </span>
+            <span className="text-base">네이버 아이디로 시작</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
