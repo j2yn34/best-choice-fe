32e72d5712be2adb6c861f601272a31c5f78b7c1
Jiyeon Park <wldusys34@gmail.com>
2023-08-25 21:40:39 +0900

    Feat: Not found Page, Error Page 페이지 분리
    - 없는 경로로 접근했을 경우 라우터로 Not Found 페이지 처리, 메인으로 버튼
    - 있는 페이지에서 오류 난 경우 ErrorBoundary로 에러페이지 처리, 새로고침 버튼

--- src/views/NotFoundPage.tsx
+++ src/views/NotFoundPage.tsx
@@ -1,22 +1,20 @@
-const ErrorPage = () => {
-  const onReloadClick = () => {
-    location.reload();
-  };
+import { Link } from "react-router-dom";
 
+const NotFoundPage = () => {
   return (
     <div className="flex flex-col items-center justify-center pt-8">
       <div className="text-center mb-12">
         <p className="text-6xl font-bold mb-2">404</p>
-        <p className="text-xl">앗! 페이지를 로드할 수 없어요.</p>
+        <p className="text-xl">앗! 페이지를 찾을 수 없어요.</p>
       </div>
-      <button
-        onClick={onReloadClick}
+      <Link
+        to="/"
         className="btn bg-black-primary text-white hover:bg-black px-6"
       >
-        새로고침
-      </button>
+        메인으로
+      </Link>
     </div>
   );
 };
 
-export default ErrorPage;
+export default NotFoundPage;
