d70c84e1b97230ad8f2897be1012d7360ff2b8c1
Jiyeon Park <wldusys34@gmail.com>
2023-08-25 19:12:17 +0900

    Feat: 없는 페이지 경로로 이동했을 때 404 페이지 구현
    - 있는 페이지에서의 오류인지, 없는 페이지인지 여부에 따라 문구 및 버튼 조건부 처리

--- src/views/ErrorPage.tsx
+++ src/views/ErrorPage.tsx
@@ -1,20 +1,38 @@
+import { Link, useLocation } from "react-router-dom";
+
 const ErrorPage = () => {
   const onReloadClick = () => {
     location.reload();
   };
 
+  const { pathname } = useLocation();
+  const isNotFound = pathname === "*";
+
   return (
     <div className="flex flex-col items-center justify-center pt-8">
       <div className="text-center mb-12">
         <p className="text-6xl font-bold mb-2">404</p>
-        <p className="text-xl">앗! 페이지를 로드할 수 없어요.</p>
+        {isNotFound ? (
+          <p className="text-xl">앗! 페이지를 로드할 수 없어요.</p>
+        ) : (
+          <p className="text-xl">앗! 페이지를 찾을 수 없어요.</p>
+        )}
       </div>
-      <button
-        onClick={onReloadClick}
-        className="btn bg-black-primary text-white hover:bg-black px-6"
-      >
-        새로고침
-      </button>
+      {isNotFound ? (
+        <button
+          onClick={onReloadClick}
+          className="btn bg-black-primary text-white hover:bg-black px-6"
+        >
+          새로고침
+        </button>
+      ) : (
+        <Link
+          to="/"
+          className="btn bg-black-primary text-white hover:bg-black px-6"
+        >
+          메인으로
+        </Link>
+      )}
     </div>
   );
 };
