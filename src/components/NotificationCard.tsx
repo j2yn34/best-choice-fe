770ce0b7cc9298c5085baf37408351f7f2a5b372
Jiyeon Park <wldusys34@gmail.com>
2023-08-26 19:32:00 +0900

    Feat: 알림메시지 스켈레톤UI 제작 및 알림페이지 Suspense 적용

--- src/components/NotificationCard.tsx
+++ src/components/NotificationCard.tsx
@@ -7,11 +7,14 @@ const NotificationCard = (): JSX.Element => {
   const [showModal, setShowModal] = useState(false);
   const [clickedData, setClickedData] = useState<number | null>(null);
 
-  const {
-    isLoading,
-    data: notificationData,
-    isError,
-  } = useFetchData("/notificationData", ["notificationData"]);
+  const { data: notificationData, isError } = useFetchData(
+    "/notificationData",
+    ["notificationData"]
+  );
+
+  if (isError) {
+    console.log("데이터 불러오기 실패");
+  }
 
   const openModal = (data: Notification) => {
     setShowModal(true);
@@ -25,42 +28,34 @@ const NotificationCard = (): JSX.Element => {
     document.body.style.overflow = "auto";
   };
 
-  if (isError) {
-    console.log("데이터 불러오기 실패");
-  }
-
   return (
     <>
-      {isLoading ? (
-        "Loading..."
-      ) : (
-        <>
-          {notificationData.map((data: Notification) => (
-            <div key={data.notificationId}>
-              <div className="flex justify-between pt-8 mb-4">
-                <div>{data.createdDate}</div>
-                <div>
-                  <button>삭제</button>
-                </div>
-              </div>
-              <div
-                tabIndex={0}
-                onClick={() => openModal(data)}
-                className={`flex flex-col md:flex-row items-center w-full min-h-[96px] p-5 mb-8 bg-white rounded-2xl shadow-md cursor-pointer text-center ${
-                  data.checked ? "opacity-50" : ""
-                }`}
-              >
-                <span className="text-lg font-semibold mr-3">
-                  "{data.postTitle}"
-                </span>
-                <span className="text-sm mt-1.5 md:mt-0">
-                  투표글의 채팅방이 개설되었어요.
-                </span>
+      <>
+        {notificationData.map((data: Notification) => (
+          <div key={data.notificationId}>
+            <div className="flex justify-between pt-8 mb-4">
+              <div>{data.createdDate}</div>
+              <div>
+                <button>삭제</button>
               </div>
             </div>
-          ))}
-        </>
-      )}
+            <div
+              tabIndex={0}
+              onClick={() => openModal(data)}
+              className={`flex flex-col md:flex-row items-center w-full min-h-[96px] p-5 mb-8 bg-white rounded-2xl shadow-md cursor-pointer text-center ${
+                data.checked ? "opacity-50" : ""
+              }`}
+            >
+              <span className="text-lg font-semibold mr-3">
+                "{data.postTitle}"
+              </span>
+              <span className="text-sm mt-1.5 md:mt-0">
+                투표글의 채팅방이 개설되었어요.
+              </span>
+            </div>
+          </div>
+        ))}
+      </>
       {showModal ? (
         <EnterChatRoom postId={clickedData} closeModal={closeModal} />
       ) : null}
--- src/views/NotificationPage.tsx
+++ src/views/NotificationPage.tsx
@@ -1,13 +1,20 @@
-import NotificationCard from "../components/NotificationCard";
+import React, { Suspense } from "react";
+import LoadNotification from "../components/skeletonUI/LoadNotification";
 
 const NotificationPage = (): JSX.Element => {
+  const NotificationCard = React.lazy(
+    () => import("../components/NotificationCard")
+  );
+
   return (
     <>
       <div className="flex items-center justify-between">
         <h1 className="text-2xl font-semibold">채팅방 개설 알림</h1>
         <button>전체 삭제</button>
       </div>
-      <NotificationCard />
+      <Suspense fallback={<LoadNotification />}>
+        <NotificationCard />
+      </Suspense>
     </>
   );
 };
