033f3f26cea2d4e4cdc171473cc6bd195bf3df07
Hansebin <sebinn052@gmail.com>
2023-08-28 18:21:22 +0900

    Refactor: 미리 보기 기능 구현을 위해 사용하던 2개의 state 1개로 관리
    - 이미지와 동영상 미리 보기 기능을 위해서 사용하던 2개의 state를 하나로 합쳐서 관리할 수 있도록 코드 수정
    - 6개 이상의 이미지, 동영상을 업로드할 때 보여줘야 하는 안내 문구창 기본 모달 컴포넌트로 설정

--- src/components/create/FileInput.tsx
+++ src/components/create/FileInput.tsx
@@ -2,34 +2,59 @@ import { ChangeEvent, useState } from "react";
 import { useRecoilState, RecoilState } from "recoil";
 import { inputValueState } from "../../states/recoil";
 import { InputValue } from "../../states/recoilType";
+import BasicModal from "../modal/BasicModal";
 
 const maxFileCnt = 5;
 
 const FileInput = () => {
-  const [showImages, setShowImages] = useState<string[]>([]);
-  const [showVideos, setShowVideos] = useState<string[]>([]);
+  const [media, setMedia] = useState<{
+    images: string[] | null;
+    videos: string[] | null;
+  }>({
+    images: null,
+    videos: null,
+  });
+  const [showModal, setShowModal] = useState<boolean>(false);
   const [, setInputValue] = useRecoilState(
     inputValueState as RecoilState<InputValue>
   );
 
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
+  const openModal = () => {
+    setShowModal(true);
+    document.body.style.overflow = "hidden";
+  };
 
+  const closeModal = () => {
+    setShowModal(false);
+    document.body.style.overflow = "auto";
+  };
+
+  const processFiles = (files: FileList) => {
     if (files && files?.length > maxFileCnt) {
-      alert("첨부파일은 최대 " + maxFileCnt + "개 까지 첨부 가능합니다.");
+      openModal();
+
+      setInputValue((prevInputValues) => ({
+        ...prevInputValues,
+        files: null,
+      }));
+
       return;
     }
 
-    if (files) {
-      setShowImages([]);
+    const newMedia: { images: string[]; videos: string[] } = {
+      images: [],
+      videos: [],
+    };
 
+    if (files) {
       for (let i = 0; i < files.length; i++) {
-        if (files[i].type.includes("video")) {
-          const currentVideoUrl = URL.createObjectURL(files[i]);
-          setShowVideos((prevImages) => [...prevImages, currentVideoUrl]);
+        const file = files[i];
+        const url = URL.createObjectURL(file);
+
+        if (file.type.includes("video")) {
+          newMedia.videos.push(url);
         } else {
-          const currentImageUrl = URL.createObjectURL(files[i]);
-          setShowImages((prevImages) => [...prevImages, currentImageUrl]);
+          newMedia.images.push(url);
         }
       }
 
@@ -37,15 +62,28 @@ const FileInput = () => {
 
       setInputValue((prevInputValues) => ({
         ...prevInputValues,
-        files: fileList.slice(0, 5),
+        files: fileList,
       }));
+    }
+
+    setMedia(newMedia);
+  };
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files) {
+      setMedia({ images: null, videos: null });
+      processFiles(files);
     }
   };
 
   return (
     <>
-      <p className="text-lg mt-8">파일 첨부</p>
-      <p className="mb-4 text-sm text-gray">* 최대 5개 업로드 가능</p>
+      <div className="flex items-center text-lg mb-4 mt-8">
+        <p>파일 첨부</p>
+        <p className="ml-2 text-xs text-gray">* 최대 5개 업로드 가능</p>
+      </div>
+
       <input
         type="file"
         multiple
@@ -53,17 +91,23 @@ const FileInput = () => {
         className="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:cursor-pointer file:border-0 file:text-sm file:font-semibold file:bg-color-bg file:text-black"
       />
       <div className="flex items-center md:gap-5 mt-3 gap-1">
-        {showImages.map((image, id) => (
+        {media.images?.map((image, id) => (
           <div key={id}>
             <img src={image} alt={`${image}-${id}`} width={300} />
           </div>
         ))}
-        {showVideos.map((video, id) => (
+        {media.videos?.map((video, id) => (
           <div key={id}>
             <video src={video} controls width={300} />
           </div>
         ))}
       </div>
+      {showModal ? (
+        <BasicModal
+          message={`파일 업로드는 최대 ${maxFileCnt}까지만 가능합니다. `}
+          closeModal={closeModal}
+        />
+      ) : null}
     </>
   );
 };
