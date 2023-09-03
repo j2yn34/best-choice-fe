52ad269d6d975b8b51809cf6b0163e7fed1427cd
Jiyeon Park <wldusys34@gmail.com>
2023-09-03 23:57:02 +0900

    Refactor: 회원페이지에서 회원 관련 정렬 데이터 요청 코드 수정
    - 회원페이지에서 토큰 넣어서 데이터 요청 및 정렬 기능 구현
    - 프롭스로 받은 토큰 여부에 따라 PostCardList에서 회원데이터와 일반데이터 요청 분기 처리

--- src/views/MemberPage.tsx
+++ src/views/MemberPage.tsx
@@ -1,27 +1,39 @@
-import { Suspense, lazy, useState, useCallback } from "react";
+import { Suspense, lazy, useState } from "react";
 import ChangeNickname from "../components/modal/ChangeNickname";
 import LoadPostCard from "../components/skeletonUI/LoadPostCard";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorMessage from "../components/common/ErrorMessage";
+import { useRecoilState } from "recoil";
+import { accessTokenState } from "../states/recoil";
+import { useNavigate } from "react-router-dom";
+// import useFetchData from "../hooks/useFetchData";
 
 const sortNames = [
-  { name: "작성한 투표글", message: "작성한 투표글 클릭" },
-  { name: "투표한 글", message: "투표한 글 클릭" },
-  { name: "추천한 글", message: "추천한 글 클릭" },
-  { name: "댓글 단 글", message: "댓글 단 글 클릭" },
+  { name: "작성한 투표글", sort: "POSTS" },
+  { name: "투표한 글", sort: "CHOICES" },
+  { name: "추천한 글", sort: "LIKES" },
+  { name: "댓글 단 글", sort: "COMMENTS" },
 ];
 
 const MemberPage = (): JSX.Element => {
   const [showModal, setShowModal] = useState(false);
+  const [postSort, setPostSort] = useState<string>("POSTS");
 
   const PostCardList = lazy(
     () => import("../components/contents/PostCardList")
   );
 
-  const clickSort = useCallback((message: string) => {
-    console.log("클릭 성공!");
-    alert(message);
-  }, []);
+  const [token, setToken] = useRecoilState<string>(accessTokenState);
+  const navigate = useNavigate();
+
+  const onLogoutClick = () => {
+    const ok = confirm("로그아웃 할까요?");
+    if (ok) {
+      setToken("");
+      navigate("/");
+      alert("로그아웃 되었어요. 또 만나요!");
+    } else return;
+  };
 
   const openModal = () => {
     setShowModal(true);
@@ -33,12 +45,19 @@ const MemberPage = (): JSX.Element => {
     document.body.style.overflow = "auto";
   };
 
+  // const { data: memberData } = useFetchData(
+  //   "api/members/mypage",
+  //   ["memberData"],
+  //   token
+  // );
+
   return (
     <>
       <div className="flex justify-between items-center mb-8">
         <div className="flex items-center">
           <p className="text-2xl mr-6">
-            안녕하세요, <span className="font-semibold">닉네임 </span>님!
+            안녕하세요,<span className="font-semibold">닉네임 </span>님!
+            {/* <span className="font-semibold">${memberData.nickname} </span>님! */}
           </p>
           <button
             onClick={() => openModal()}
@@ -47,18 +66,18 @@ const MemberPage = (): JSX.Element => {
             닉네임 변경
           </button>
         </div>
-        <button className="font-bold">로그아웃</button>
+        <button onClick={onLogoutClick} className="font-bold">
+          로그아웃
+        </button>
       </div>
       <ul className="flex items-center gap-4 mb-7">
         {sortNames.map((sortName) => (
           <li
             key={sortName.name}
             className={`cursor-pointer ${
-              sortName.name === "작성한 투표글"
-                ? "text-blue-dark font-semibold"
-                : ""
+              sortName.sort === postSort ? "text-blue-dark font-semibold" : ""
             }`}
-            onClick={() => clickSort(sortName.message)}
+            onClick={() => setPostSort(sortName.sort)}
             tabIndex={0}
           >
             {sortName.name}
@@ -67,7 +86,7 @@ const MemberPage = (): JSX.Element => {
       </ul>
       <ErrorBoundary FallbackComponent={ErrorMessage}>
         <Suspense fallback={<LoadPostCard limit={2} />}>
-          <PostCardList limit={3} />
+          <PostCardList limit={3} sort={postSort} token={token} />
         </Suspense>
       </ErrorBoundary>
       {showModal ? <ChangeNickname closeModal={closeModal} /> : null}
