6df0235f520455dec6702b491501c85328a0d955
Jiyeon Park <wldusys34@gmail.com>
2023-09-03 23:20:29 +0900

    Feat: recoil 토큰 저장 설정, 로그인 버튼 클릭 시 소셜로그인 페이지로 이동 구현
    - 로그인 시 세션스토리지에 토큰 저장할 수 있도록 recoil 설정
    - 로그인 페이지에서 로그인 버튼 클릭 시 해당 소셜 로그인 페이지로 이동 구현

--- src/views/LoginPage.tsx
+++ src/views/LoginPage.tsx
@@ -3,24 +3,51 @@ import { RiKakaoTalkFill } from "react-icons/ri";
 import { SiNaver } from "react-icons/si";
 
 const LoginPage = (): JSX.Element => {
+  const GOOGLE_AUTH_URI = "http://www.winnow-bestchoice.com:8080/login/google";
+  const KAKAO_AUTH_URI = "http://www.winnow-bestchoice.com:8080/login/kakao";
+  const NAVER_AUTH_URI = "http://www.winnow-bestchoice.com:8080/login/naver";
+
+  const onSocialLogin = (provider: string) => {
+    switch (provider) {
+      case "google":
+        window.location.href = GOOGLE_AUTH_URI;
+        break;
+      case "kakao":
+        window.location.href = KAKAO_AUTH_URI;
+        break;
+      default:
+        window.location.href = NAVER_AUTH_URI;
+        break;
+    }
+  };
+
   return (
     <div className="fixed inset-0 flex items-center justify-center mb-24">
       <div className="flex flex-col items-center gap-12">
         <span>로고</span>
         <div className="flex flex-col gap-4">
-          <button className="btn w-64 h-14 border-0 bg-white hover:bg-blue-100">
+          <button
+            onClick={() => onSocialLogin("google")}
+            className="btn w-64 h-14 border-0 bg-white hover:bg-blue-100"
+          >
             <span className="text-lg">
               <FcGoogle />
             </span>
             <span className="text-base">구글 아이디로 시작</span>
           </button>
-          <button className="btn w-64 h-14 border-0 bg-[#fee500] hover:bg-[#fada0a]">
+          <button
+            onClick={() => onSocialLogin("kakao")}
+            className="btn w-64 h-14 border-0 bg-[#fee500] hover:bg-[#fada0a]"
+          >
             <span className="text-lg">
               <RiKakaoTalkFill />
             </span>
             <span className="text-base">카카오 아이디로 시작</span>
           </button>
-          <button className="btn w-64 h-14 border-0 bg-[#03c75a] hover:bg-[#17b75e]">
+          <button
+            onClick={() => onSocialLogin("naver")}
+            className="btn w-64 h-14 border-0 bg-[#03c75a] hover:bg-[#17b75e]"
+          >
             <span className="text-white">
               <SiNaver />
             </span>
