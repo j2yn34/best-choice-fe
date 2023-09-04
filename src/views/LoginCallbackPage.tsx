bfd0386b7a55783172fc0f958681b85399df7625
Jiyeon Park <wldusys34@gmail.com>
2023-09-04 16:29:23 +0900

    Feat: 로그인 시 유저 정보 recoil 전역 상태로 저장
    - 로그인 시 받은 토큰으로 유저 정보 데이터 요청
    - memberId, nickname 값 전역 상태로 저장

--- src/views/LoginCallbackPage.tsx
+++ src/views/LoginCallbackPage.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { accessTokenState } from "../states/recoil";
+import { accessTokenState, userDataState } from "../states/recoil";
 import { useSetRecoilState } from "recoil";
 import BasicModal from "../components/modal/BasicModal";
+import { UserDataState } from "../states/recoilType";
+import axios from "axios";
 
 const LoginCallbackPage = () => {
   const setAccessToken = useSetRecoilState<string>(accessTokenState);
+  const setUserData = useSetRecoilState<UserDataState>(userDataState);
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(true);
   const [showModal, setShowModal] = useState<boolean>(false);
@@ -13,16 +16,34 @@ const LoginCallbackPage = () => {
   useEffect(() => {
     const token = new URL(window.location.href).searchParams.get("token");
 
+    const getUserData = async (token: string) => {
+      try {
+        const response = await axios({
+          method: "get",
+          url: "/api/api/members/mypage",
+          headers: { Authorization: `Bearer  ${token}` },
+          responseType: "json",
+        });
+
+        const { memberId, nickname } = response.data;
+        setUserData({ memberId, nickname });
+      } catch (error) {
+        console.error(error);
+      }
+      return;
+    };
+
     if (token) {
       setIsLoading(false);
       setAccessToken(token);
+      getUserData(token);
       navigate("/");
       alert("반가워요! 로그인 되었어요.");
     } else {
       setIsLoading(false);
       setShowModal(true);
     }
-  }, [navigate, setAccessToken]);
+  }, [navigate, setAccessToken, setUserData]);
 
   const closeModal = () => {
     setShowModal(false);
