bd4b192957c312ffa5c68451a9046c0d88d3b38e
Hansebin <sebinn052@gmail.com>
2023-09-14 00:59:30 +0900

    Design: InfoBar 디자인 수정
    - roomData에서 타이틀, 옵션 데이터 가져오기
    - 옵션 스타일링

--- src/components/chat/InfoBar.tsx
+++ src/components/chat/InfoBar.tsx
@@ -1,21 +1,32 @@
+import { Post } from "../../mocks/mockType";
 import { SlArrowDown } from "react-icons/sl";
 
-const InfoBar = (): JSX.Element => {
+const InfoBar = ({ roomData }: { roomData: Post }): JSX.Element => {
   return (
     <>
       <div className="absolute w-full p-2 z-20">
         <div tabIndex={0} className="collapse shadow-sm">
           <input type="checkbox" />
           <div className="collapse-title flex items-center justify-between pr-6 bg-white text-base">
-            <span>투표글 제목</span>
+            <span>{roomData.title}</span>
             <div>
               <SlArrowDown />
             </div>
           </div>
           <div className="collapse-content bg-white">
-            <p className="mb-2">투표글 제목</p>
-            <p className="py-1">A옵션</p>
-            <p className="py-1">B옵션</p>
+            <p className="mb-3">{roomData.title}</p>
+            <p>
+              <span className="text-red-dark font-bold px-2 py-1 mr-3 rounded-full text-sm md:text-base bg-red-100">
+                A
+              </span>
+              {roomData.optionA}
+            </p>
+            <p className="mt-3">
+              <span className="text-blue-dark font-bold px-2 py-1 mr-3 rounded-full text-sm md:text-base bg-blue-100">
+                B
+              </span>
+              {roomData.optionB}
+            </p>
           </div>
         </div>
       </div>
