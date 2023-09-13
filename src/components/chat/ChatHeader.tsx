99e5dbd1d9470a57af5b232719c9b32624358b1e
Jiyeon Park <wldusys34@gmail.com>
2023-09-13 14:23:19 +0900

    Design: ChatHeader, InfoBar 디자인 수정
    - 헤더에 나가기 글자 추가, 색상 변경
    - InfoBar에 꺽쇠 아이콘 추가

--- src/components/chat/ChatHeader.tsx
+++ src/components/chat/ChatHeader.tsx
@@ -3,7 +3,7 @@ import ChatUserBadge from "../common/ChatUserBadge";
 import { useNavigate } from "react-router";
 
 const Header = ({
-  roomId,
+  // roomId,
   exit,
 }: {
   roomId: string;
@@ -12,18 +12,21 @@ const Header = ({
   const navigate = useNavigate();
   return (
     <>
-      <div className="flex items-center justify-between h-16 px-2 bg-white border-b border-gray/[0.2]">
-        <button
-          onClick={() => {
-            exit();
-            navigate(-1);
-          }}
-          className="text-2xl"
-        >
-          <BiArrowBack />
-        </button>
-        <div>{roomId}번 채팅방</div>
         <ChatUserBadge ChatUserCount={1} />
+      <div className="flex items-center justify-between h-16 px-2 bg-blue-200 drop-shadow-sm">
+        <div className="flex items-center">
+          <button
+            onClick={() => {
+              exit();
+              navigate(-1);
+            }}
+            className="text-2xl mr-1"
+          >
+            <BiArrowBack />
+          </button>
+          <span className="text-sm">나가기</span>
+        </div>
+        {/* <div>{roomId}번 채팅방</div> */}
       </div>
     </>
   );
--- src/components/chat/InfoBar.tsx
+++ src/components/chat/InfoBar.tsx
@@ -1,12 +1,17 @@
-// 채팅 주제(투표 title), 옵션A, B
+import { SlArrowDown } from "react-icons/sl";
 
 const InfoBar = (): JSX.Element => {
   return (
     <>
-      <div className="absolute w-full p-1 z-20">
-        <div tabIndex={0} className="collapse bg-base-200 shadow-sm">
+      <div className="absolute w-full p-2 z-20">
+        <div tabIndex={0} className="collapse shadow-sm">
           <input type="checkbox" />
-          <div className="collapse-title bg-white text-base">투표글 제목</div>
+          <div className="collapse-title flex items-center justify-between pr-6 bg-white text-base">
+            <span>투표글 제목</span>
+            <div>
+              <SlArrowDown />
+            </div>
+          </div>
           <div className="collapse-content bg-white">
             <p className="mb-2">투표글 제목</p>
             <p className="py-1">A옵션</p>
