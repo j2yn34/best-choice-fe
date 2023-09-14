8b15a4e3bb6a54f8224df4090b9284849af1462b
Jiyeon Park <wldusys34@gmail.com>
2023-09-14 16:35:18 +0900

    Feat: 알림 메시지 데이터 가져오기 및 알림 메시지 삭제 기능 구현
    - 알림 페이지 들어오면 알림 표시 false 처리
    - 알림 메시지 리스트 데이터 불러오기
    - 알림 메시지 개별 삭제 및 전체 삭제 기능 구현 및 모달 팝업

--- src/components/contents/NotificationCard.tsx
+++ src/components/contents/NotificationCard.tsx
@@ -1,49 +1,99 @@
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
+import axios from "axios";
 import useFetchData from "../../hooks/useFetchData";
-import EnterChatRoom from "../modal/EnterChatRoom";
+import NotificationEnterChatRoom from "../modal/NotificationEnterChatRoom";
 import { Notification } from "../../mocks/mockType";
 import NoDataMessage from "../common/NoDataMessage";
+import { useRecoilValue } from "recoil";
+import { accessTokenState } from "../../states/recoil";
+import BasicModal from "../modal/BasicModal";
 
 const NotificationCard = (): JSX.Element => {
-  const [showModal, setShowModal] = useState(false);
+  const [showChatEnterModal, setShowChatEnterModal] = useState<boolean>(false);
+  const [showDeleteAllModal, setShowDeleteAllModal] = useState<boolean>(false);
   const [clickedData, setClickedData] = useState<number | null>(null);
+  const token = useRecoilValue<string>(accessTokenState);
 
   const { data: notificationData } = useFetchData(
-    "/notificationData",
+    "/api/api/notifications/all?page=0",
     ["notificationData"],
-    ""
+    token
   );
 
-  if (notificationData["content"].length === 0) {
+  if (notificationData["content"].length <= 0) {
     return <NoDataMessage message="새로운 알림이 없어요" />;
   }
 
-  const openModal = (data: Notification) => {
-    setShowModal(true);
-    setClickedData(data.postId);
+  const openChatEnterModal = (data: Notification) => {
+    setShowChatEnterModal(true);
+    setClickedData(data.notificationId);
     document.body.style.overflow = "hidden";
   };
 
-  const closeModal = () => {
-    setShowModal(false);
-    setClickedData(null);
+  const openDeleteAllModal = () => {
+    setShowDeleteAllModal(true);
+    document.body.style.overflow = "hidden";
+  };
+
+  const closeModal = (setFunc: Dispatch<SetStateAction<boolean>>) => {
+    setFunc(false);
     document.body.style.overflow = "auto";
+  };
+
+  const onDeleteAllNoticeClick = async () => {
+    try {
+      await axios({
+        method: "delete",
+        url: `/api/api/notifications`,
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      location.reload();
+      closeModal(setShowDeleteAllModal);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onDeleteNoticeClick = async (data: Notification) => {
+    try {
+      await axios({
+        method: "delete",
+        url: `/api/api/notifications/${data.notificationId}`,
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      location.reload();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <>
       <>
+        <div className="flex w-full justify-end mb-3">
+          <button onClick={openDeleteAllModal} className="text-red-dark">
+            전체 삭제
+          </button>
+        </div>
+
         {notificationData["content"].map((data: Notification) => (
           <div key={data.notificationId}>
-            <div className="flex justify-between mb-4">
+            <div className="flex justify-between mb-3">
               <div>{data.createdDate}</div>
               <div>
-                <button>삭제</button>
+                <button onClick={() => onDeleteNoticeClick(data)}>삭제</button>
               </div>
             </div>
+
             <div
               tabIndex={0}
-              onClick={() => openModal(data)}
+              onClick={() => openChatEnterModal(data)}
               className={`flex flex-col md:flex-row items-center w-full min-h-[96px] p-5 mb-8 bg-white rounded-2xl shadow-md cursor-pointer text-center ${
                 data.checked ? "opacity-50" : ""
               }`}
@@ -58,8 +108,18 @@ const NotificationCard = (): JSX.Element => {
           </div>
         ))}
       </>
-      {showModal && (
-        <EnterChatRoom postId={clickedData} closeModal={closeModal} />
+      {showChatEnterModal && (
+        <NotificationEnterChatRoom
+          notificationId={clickedData}
+          closeModal={() => closeModal(setShowChatEnterModal)}
+        />
+      )}
+      {showDeleteAllModal && (
+        <BasicModal
+          message={"전체 알림을 삭제할까요?"}
+          closeModal={() => closeModal(setShowDeleteAllModal)}
+          confirm={onDeleteAllNoticeClick}
+        />
       )}
     </>
   );
